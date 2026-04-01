3da0b051b0f3fd473f6dfff160ff6b05740604c3
Tunan10 <tunansakib200@gmail.com>
2026-04-01 13:05:36 +0600

    bug fixxed

--- src/Components/BannerFooter.jsx
+++ src/Components/BannerFooter.jsx
@@ -5,18 +5,18 @@ const BannerFooter = () => {
     <div className='w-full h-64 bg-linear-to-r from-blue-500 to-purple-500 flex justify-around items-center'>
 
       <div className='text-center'>
-        <h2 className='font-bold text-6xl text-white '>50K+</h2>
+        <h2 className='font-bold text-2xl sm:text-6xl text-white '>50K+</h2>
         <p className='text-white font-medium'>Active Users</p>
       </div>
         <div className="divider divider-horizontal py-20 "></div>
 
       <div className='text-center'>
-        <h2 className='font-bold text-6xl text-white '>200+</h2>
+        <h2 className='font-bold text-2xl sm:text-6xl text-white '>200+</h2>
         <p className='text-white font-medium'>Premium Tools</p>
       </div>
        <div className="divider divider-horizontal py-20 "></div>
       <div className='text-center'>
-        <h2 className='font-bold text-6xl text-white '>4.9</h2>
+        <h2 className='font-bold text-2xl sm:text-6xl text-white '>4.9</h2>
         <p className='text-white font-medium'>Rating</p>
       </div>
       
